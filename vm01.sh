73473d54c6aa0b03c1bebbb219a55b301dbbe849
lzddr <andreeki@usc.edu>
2018-06-20 15:02:46 -0700

    06/20/2018

--- vm01.sh
+++ vm01.sh
@@ -2,3 +2,5 @@ plink --bfile /auto/pmd-02/figi/andreeki/Data/Victor_RNAseq/Genotypes/pre-imputa
 
 # 0.18 should filter out 9 samples
 plink --bfile /auto/pmd-02/figi/andreeki/Data/Victor_RNAseq/Genotypes/pre-imputation/FIGI_merged  --mind 0.18  --make-bed --out FIGI_tmp
+
+awk '{if($6 > 0.18) {print}}' tmp2.imiss > excluded_samples.txt
